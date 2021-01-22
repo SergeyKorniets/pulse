47fea59f674f30e367e3444cc2822dfef4c5f01f
Sergey <gravekeeperof@gmail.com>
2021-01-22 19:31:02 +0300

    ade slider

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true
+        prevArrow: "<button type='button' class='slick-prev'><img src='icons/left.png'></button>",
+        nextArrow: "<button type='button' class='slick-next'><img src='icons/right.png'></button>",
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
