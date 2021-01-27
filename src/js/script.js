40fb4fff2a56e6be4e92093e9b61ddf461767aba
Korniets S. Nickolaevich <gravekeeperofficial@gmail.com>
2021-01-27 19:55:35 +0300

    moded modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -49,5 +49,37 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         });
     });
-   
+
+    function valideForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя!",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона!",
+                email: {
+                    required: "Пожалуйста, введите свою почту!",
+                    email: "Неправильно введен адрес почты!"
+                }
+            }
+        });
+    };
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
