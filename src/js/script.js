dc881ffdd88dd6ac6690786b77954e8c7d91150d
Sergey <gravekeeperof@gmail.com>
2021-01-27 22:55:12 +0300

    moded

--- src/js/script.js
+++ src/js/script.js
@@ -82,4 +82,38 @@ $(document).ready(function(){
     valideForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // $('form').submit(function(e){
+    //     e.preventDefault();
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "mailer/smart.php",
+    //         data: $(this).serialize()
+    //     }).done(function(){
+    //         $(this).find("input").val("");
+    //         $('#consultation, #order').fadeOut();
+    //         $('.overlay, #thanks').fadeIn('slow');
+
+
+    //         $('form').trigger('reset');
+    //     });
+    //     return false;
+    // }); 
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn('slow');
+        } else {
+            $('.pageup').fadeOut('slow');
+        }
+    });
+
+    $("a[href^='#']").click(function() {
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
 });
